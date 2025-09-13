c9d86e8a6fd0e694c8ffba84756a243f274f043a
defectcode <paladuta05@gmail.com>
2025-09-13 09:15:27 +0300

    Fix error with redis session

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,10 @@
-import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto'
 import { UserService } from 'src/user/user.service'
 import { AuthMethod, User } from 'prisma/__generated__'
 import { Request } from 'express'
+import { LoginDto } from './dto/login.dto'
+import { verify } from 'argon2'
 
 @Injectable()
 export class AuthService {
@@ -30,31 +32,40 @@ export class AuthService {
         return this.saveSession(req, newUser)
     }
 
-    public async login() {
+    public async login(req: Request, dto: LoginDto) {
+        const user = await this.userService.findByEmail(dto.email)
 
+        if(!user || !user.password) {
+            throw new NotFoundException(
+                'User is not found. Please get correct data for user'
+            )
+        }
+
+        const isValidPassword = await verify(user.password, dto.password)
+
+        if(!isValidPassword) {
+            throw new UnauthorizedException('Password is not correct.')
+        }
+
+        return this.saveSession(req, user)
     }
 
-    public async logout() {
-
-    }
-
-    private async saveSession(req: Request, user: User) {
+    public async logout(req: Request, res: Response): Promise<void> {
         return new Promise((resolve, reject) => {
-            req.session.userId = user.id
-
-            req.session.save(err => {
+            req.session.destroy(err => {
                 if(err) {
                     return reject(
                         new InternalServerErrorException(
-                            'No valid session. Please provide valid session and required parameters.'
+                            'Session Error.'
                         )
                     )
                 }
-
-                resolve({
-                    user
-                })
+                res.clearCookie('session')
             })
         })
+    }
+
+    private async saveSession(req: Request, user: User) {
+        console.log('Session saved. User: ', user)
     }
 }
